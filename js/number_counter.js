9762097360e6482760feeafdccc254195cc7a220
mjr68 <matthewrose1799@gmail.com>
2020-07-05 12:42:30 -0400

    fixed animation for stats

--- js/number_counter.js
+++ js/number_counter.js
@@ -1,29 +1,29 @@
 var a = 0;
 $(window).scroll(function () {
-  var oTop = $('#counter').offset().top - window.innerHeight;
-  if (a == 0 && $(window).scrollTop() > oTop) {
-    $('.counter-value').each(function () {
-      var $this = $(this),
-        countTo = $this.attr('data-count');
+  $('.counter-value').each(function(index) {
+    var $this = $(this)
+    var oTop = $this.offset().top - window.innerHeight;
+    if(a == index && $(window).scrollTop() > oTop) {
+      countTo = $this.attr('data-count');
       $({
         countNum: $this.text(),
       }).animate(
         {
           countNum: countTo,
         },
-
         {
           duration: 3000,
           easing: 'swing',
           step: function () {
             $this.text(Math.floor(this.countNum));
+            console.log(this.countNum)
           },
           complete: function () {
             $this.text(this.countNum);
           },
         }
       );
-    });
-    a = 1;
-  }
+      a +=1;
+    }
+  })
 });
