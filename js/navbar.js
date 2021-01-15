35148dfa47ff5991a6ff4760acfb0956b06f6c9d
Matt Rose <matthewrose1799@gmail.com>
2021-01-15 01:34:35 -0500

    cleaned up jquery, margins on iPad view

--- js/navbar.js
+++ js/navbar.js
@@ -3,15 +3,16 @@ var lastScrollId,
     topMenu = $("#navOptions"),
     topMenuHeight = topMenu.outerHeight(),
     // All list items
-    menuItems = topMenu.find("a"),
+    menuItems = topMenu.find("a.menu-item"),
+    subMenuItems = topMenu.find("a.sub-item"),
     // Anchors corresponding to menu items
     scrollItems = menuItems.map(function(){
       var item = $($(this).attr("href"));
-      if (item.length && !$(item[0].parentElement).attr("id")) { return item; }
+      if (item.length) { return item; }
     }),
-    subItems = menuItems.map(function(){
+    subItems = subMenuItems.map(function(){
       var item = $($(this).attr("href"));
-      if (item.length && $(item[0].parentElement).attr("id")) { return item; }
+      if (item.length) { return item; }
     });
 
 
@@ -21,33 +22,31 @@ $(window).scroll(function() {
    var fromTop = $(this).scrollTop() + topMenuHeight
    
    // Get id of current scroll item
-   var cur = scrollItems.map(function(){
+   var currentElem = scrollItems.map(function(){
      if ($(this).offset().top < fromTop)
        return this;
    });
    // Get the id of the current element
-   cur = cur[cur.length-1];
-   var id = cur && cur.length ? cur[0].id : "";
+   currentElem = currentElem[currentElem.length-1];
+   var scrollId = currentElem && currentElem.length ? currentElem[0].id : "";
    
-   if (lastScrollId !== id) {
+   if (lastScrollId !== scrollId) {
        // Set/remove active class
        menuItems.parent().end().filter("[href='#"+lastScrollId+"']").parent().removeClass("active");
-       lastScrollId = id;
-       menuItems.parent().end().filter("[href='#"+id+"']").parent().addClass("active");
+       lastScrollId = scrollId;
+       menuItems.parent().end().filter("[href='#"+scrollId+"']").parent().addClass("active");
    }  
 
-  var subCur = subItems.map(function(){
+  var subCurrentElem = subItems.map(function(){
      if ($(this).offset().top < fromTop)
        return this;
    });
-   // Get the id of the current element
-   subCur = subCur[subCur.length-1];
-   var subId = subCur && subCur.length ? subCur[0].id : "";
+   subCurrentElem = subCurrentElem[subCurrentElem.length-1];
+   var subId = subCurrentElem && subCurrentElem.length ? subCurrentElem[0].id : "";
    
    if (lastSubId !== subId) {
-       // Set/remove active class
-       menuItems.parent().end().filter("[href='#"+lastSubId+"']").removeClass("active");
+       subMenuItems.parent().end().filter("[href='#"+lastSubId+"']").removeClass("active");
        lastSubId = subId;
-       menuItems.parent().end().filter("[href='#"+subId+"']").addClass("active");
+       subMenuItems.parent().end().filter("[href='#"+subId+"']").addClass("active");
    }  
 });
